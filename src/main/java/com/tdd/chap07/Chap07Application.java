131bbdc0d4ca9e947a400a198a77b1cfd1ea8b40
wwwkang8 <wwwkang8@gmail.com>
2024-03-09 10:49:07 +0900

    첫 커밋

---
+++ src/main/java/com/tdd/chap07/Chap07Application.java
@@ -0,0 +1,13 @@
+package com.tdd.chap07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap07Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Chap07Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/tdd/chap07/Chap07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.chap07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap07ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
